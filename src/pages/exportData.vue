d4e308ddb0d30e1fc7f2ded9f995eb1a7cabab71
Adele Truscott <adele.truscott2@gmail.com>
2025-05-25 13:01:20 +0200

    initial commit. new page included

---
+++ src/pages/exportData.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>Export data page</div>
+</template>
+<script lang="ts">
+import { Component, Vue, toNative } from "vue-facing-decorator";
+
+@Component
+class exportData extends Vue {
+}
+export default toNative(exportData);
+</script>
+<style></style>
